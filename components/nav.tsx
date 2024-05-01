98e66b181fa06e72116456a4c66e81f5198b1aa1
Nazar Tolstoy <nazartolstoy@MacBook-Air-Nazar.local>
2024-05-01 13:54:56 +0300

    setting fixes

--- components/nav.tsx
+++ components/nav.tsx
@@ -5,56 +5,56 @@ export default function Nav() {
     <nav>
       <Head>
         <title>Next Typescript PWA Starter</title>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet='utf-8' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          name='viewport'
+          content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
         />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
+        <meta name='description' content='Description' />
+        <meta name='keywords' content='Keywords' />
 
         {/* Android */}
-        <meta name="theme-color" content="red" />
-        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name='theme-color' content='red' />
+        <meta name='mobile-web-app-capable' content='yes' />
 
         {/* iOS */}
-        <meta name="apple-mobile-web-app-title" content="Application Title" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name='apple-mobile-web-app-title' content='Application Title' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
 
         {/* Windows  */}
-        <meta name="msapplication-navbutton-color" content="red" />
-        <meta name="msapplication-TileColor" content="red" />
-        <meta name="msapplication-TileImage" content="ms-icon-144x144.png" />
-        <meta name="msapplication-config" content="browserconfig.xml" />
+        <meta name='msapplication-navbutton-color' content='red' />
+        <meta name='msapplication-TileColor' content='red' />
+        <meta name='msapplication-TileImage' content='ms-icon-144x144.png' />
+        <meta name='msapplication-config' content='browserconfig.xml' />
 
         {/* Pinned Sites  */}
-        <meta name="application-name" content="Application Name" />
-        <meta name="msapplication-tooltip" content="Tooltip Text" />
-        <meta name="msapplication-starturl" content="/" />
+        <meta name='application-name' content='Application Name' />
+        <meta name='msapplication-tooltip' content='Tooltip Text' />
+        <meta name='msapplication-starturl' content='/' />
 
         {/* Tap highlighting  */}
-        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name='msapplication-tap-highlight' content='no' />
 
         {/* UC Mobile Browser  */}
-        <meta name="full-screen" content="yes" />
-        <meta name="browsermode" content="application" />
+        <meta name='full-screen' content='yes' />
+        <meta name='browsermode' content='application' />
 
         {/* Disable night mode for this page  */}
-        <meta name="nightmode" content="enable/disable" />
+        <meta name='nightmode' content='enable/disable' />
 
         {/* Fitscreen  */}
         {/* <meta name="viewport" content="uc-fitscreen=yes" /> */}
 
         {/* Layout mode */}
-        <meta name="layoutmode" content="fitscreen/standard" />
+        <meta name='layoutmode' content='fitscreen/standard' />
 
         {/* imagemode - show image even in text only mode  */}
-        <meta name="imagemode" content="force" />
+        <meta name='imagemode' content='force' />
 
         {/* Orientation  */}
-        <meta name="screen-orientation" content="portrait" />
+        <meta name='screen-orientation' content='portrait' />
 
         {/*  Main Link Tags  */}
         {/* <link href="favicon-16.png" rel="icon" type="image/png" sizes="16x16" />
@@ -74,18 +74,18 @@ export default function Nav() {
         {/* <link href="path/to/icon.svg" rel="mask-icon" sizes="any" color="red" /> */}
 
         {/*  Android  */}
-        <link href="icons/icon-192x192.png" rel="icon" sizes="192x192" />
+        <link href='icons/icon-192x192.png' rel='icon' sizes='192x192' />
         {/* <link href="icons/icon-128x128.png" rel="icon" sizes="128x128" /> */}
 
         {/*  Others */}
-        <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+        <link href='favicon.ico' rel='shortcut icon' type='image/x-icon' />
 
         {/*  UC Browser  */}
         {/* <link href="images/icon-52x52.png" rel="apple-touch-icon-precomposed" sizes="57x57" />
         <link href="images/icon-72x72.png" rel="apple-touch-icon" sizes="72x72" /> */}
 
         {/*  Manifest.json  */}
-        <link href="manifest.json" rel="manifest" />
+        <link href='manifest.json' rel='manifest' />
       </Head>
     </nav>
   );
