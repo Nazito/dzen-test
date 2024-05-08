f9d46e908500587430e081db005a4956edc7901e
Nazar Tolstoy <nazartolstoy@MacBook-Air-Nazar.local>
2024-05-08 18:42:32 +0300

    fix config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
-
+const runtimeCaching = require('next-pwa/cache');
 const { i18n } = require('./next-i18next.config');
 const withPWA = require('next-pwa')({
-  disable: process.env.NODE_ENV === 'development',
   dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable: process.env.NODE_ENV === 'development',
 });
 
 module.exports = withPWA({
